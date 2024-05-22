a2a42ed64ab3f4b903237388af4e38e65fc7a210
cesarneves-security <136181376+cesarneves-security@users.noreply.github.com>
2024-05-22 09:42:22 +0100

    Add files via upload
    #decrypt/#encrypt

---
+++ designer.py
@@ -0,0 +1,27 @@
+import os
+def __configure__designer():
+    os.system("clear")
+    print ("""\n
+    ___.    .__                    __
+    \_ |__  |  |    ____    ____  |  | __  ____  _______
+    | __ \ |  |   /  _ \  /  _ \ |  |/ /_/ __ \ \_  __ )
+    | \_\ \|  |__(  <_> )(  <_> )|    < \  ___/  |  | \/
+    |___  /|____/ \____/  \____/ |__|_ \ \___  > |__|
+        \/                            \/     \/ 
+    """)
+
+    print (" [N] PROGRAMADOR: César neves")
+    print (" [F] FERRAMENTA: Blooker")
+    print (" [T] TIPO: Incriptação/Decriptação")
+    print ("")
+
+def __configure__designer__sec():
+    os.system("clear")
+    print ("""\n
+    ___.    .__                    __
+    \_ |__  |  |    ____    ____  |  | __  ____  _______
+    | __ \ |  |   /  _ \  /  _ \ |  |/ /_/ __ \ \_  __ )
+    | \_\ \|  |__(  <_> )(  <_> )|    < \  ___/  |  | \/
+    |___  /|____/ \____/  \____/ |__|_ \ \___  > |__|
+        \/                            \/     \/ 
+    """)
